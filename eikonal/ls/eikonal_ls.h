4317e65dd7d267dff41e36e89dc98ddfe6a61c6c
miro3 <miroslav.kuchta@gmail.com>
2013-09-25 16:50:26 +0200

    wiki

---
+++ eikonal/ls/eikonal_ls.h
@@ -0,0 +1,10 @@
+#ifndef _EIKONAL_LS_H_
+#define _EIKONAL_LS_H_
+
+/*
+  Local solvers of eikonal equation on triangles and tetrahedrons.
+*/
+
+#include "ls_geometric.h"
+
+#endif // _EIKONAL_LS_H
---
+++ eikonal/ls/ls_geometric.h
@@ -0,0 +1,11 @@
+#ifndef _LS_GEOMETRIC_H_
+#define _LS_GEOMETRIC_H_
+
+/*
+  Geometric solvers following "FSM for eikonal equation on tring. meshes."
+*/
+
+// Given triangle vertices A, B, C with the distance u known in A, B, compute
+// u in C
+
+#define // _LS_GEOMETRIC_2D_H_
