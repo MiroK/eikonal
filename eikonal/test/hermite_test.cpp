bc8dbf274d0bbf7cf08128560e72eb7b9b4f6352
miro3 <miroslav.kuchta@gmail.com>
2013-11-10 17:58:30 +0100

    first shot on FMMHermite

--- eikonal/test/hermite_test.cpp
+++ eikonal/test/hermite_test.cpp
@@ -1,12 +1,13 @@
 #include "hermite_test.h"
 #include "gs/HermiteSolver.h"
+#include "gs/FMMHermite.h"
 
 namespace eikonal
 {
   int all_hermite_tests(std::size_t solver_type,
                         std::string ordering, std::size_t p)
   {
-    enum SOLVERS {FMM, FSM};
+    enum SOLVERS {FSM, FMM};
  
     if(solver_type == FSM)
     {
@@ -18,6 +19,18 @@ namespace eikonal
       /*run_hermite_test<HermiteSolver>("twocircle", precision, ordering, p); 
       run_hermite_test<HermiteSolver>("triangle", precision, ordering, p); 
       run_hermite_test<HermiteSolver>("zalesak", precision, ordering, p);*/ 
+    }    
+
+    if(solver_type == FMM)
+    {
+      std::cout << "Solving with fmm hermite solver:" << std::endl;
+      std::cout << "ordering = " << ordering << " L^p = " << p << std::endl;
+
+      std::size_t precision = 2;
+      run_hermite_test<FMMHermite>("point", precision, ordering, p); 
+      /*run_hermite_test<FMMHermite>("twocircle", precision, ordering, p); 
+      run_hermite_test<FMMHermite>("triangle", precision, ordering, p); 
+      run_hermite_test<FMMHermite>("zalesak", precision, ordering, p);*/ 
     }    
     return 0;
   }
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace dolfin;
 int main(int argc, char* argv[])
 {
   
-  double _P[2] = {2., -2.}; std::vector<double> P(_P, _P+2);
+  double _P[2] = {0., 0.}; std::vector<double> P(_P, _P+2);
   MyPoint point(P);
   Problem problem(point);
   
@@ -33,21 +33,24 @@ int main(int argc, char* argv[])
   Problem problem(polygon);
   */
   
-  /*RectangleMesh mesh(-2, -2, 2, 2, 2, 2, "crossed");
+  RectangleMesh mesh(-2, -2, 2, 2, 20, 20, "crossed");
   CG1::FunctionSpace V(mesh);
 
   dolfin::Function u(V);
+  dolfin::Function du_dx(V);
+  dolfin::Function du_dy(V);
+
   dolfin::Function u_exact(V);
   std::set<dolfin::la_index> fixed_dofs;
       
-  problem.init(fixed_dofs, u);
+  problem.init(fixed_dofs, u, du_dx, du_dy);
   problem.exact_solution(u_exact);
 
-  FMMSolver solver(V);
-  solver.solve(u, fixed_dofs, 0, 0); 
+  FMMHermite solver(V);
+  solver.solve(u, du_dx, du_dy, fixed_dofs, 2); 
   
   plot(u);
-  interactive(true);*/
+  interactive(true);
 
   //int status;
   // convergence test on meshes by gmsh 0 .. 6, smoothing
@@ -59,7 +62,7 @@ int main(int argc, char* argv[])
   //all_linear_tests(atoi(argv[1]), atoi(argv[2]));
  
   // HERMITE TESTS
-  enum ORDER_TYPE {CORNERS, SURFACE, DISTANCE};
+  /*enum ORDER_TYPE {CORNERS, SURFACE, DISTANCE};
   
   std::size_t solver_choice = atoi(argv[1]);
   std::size_t order_choice = atoi(argv[2]);
@@ -69,7 +72,7 @@ int main(int argc, char* argv[])
   {
     all_hermite_tests(solver_choice, "corners", p_norm);  
   }
-  /*else if(solver_choice == SURFACE)
+  else if(solver_choice == SURFACE)
   {
     all_hermite_tests("surface", p_norm);
   }
